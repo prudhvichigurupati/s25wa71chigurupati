21bcf2c82c21928130047d6d7ea2c03b492735f1
prudhvichigurupati <prudhvichigurupati88@gmail.com>
2025-03-26 13:07:55 -0500

    modified

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+let ratio = 0.75;
+let acc = 1000;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  acc *=ratio;
   res.send('respond with a resource');
 });
 
