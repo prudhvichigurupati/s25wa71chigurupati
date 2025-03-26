e2a3016231f176d154b23c15eebcd84cbf917de8
prudhvichigurupati <prudhvichigurupati88@gmail.com>
2025-03-26 13:09:38 -0500

    modified

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ let acc = 1000;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   acc *=ratio;
-  res.send('respond with a resource');
+  res.send('value is: ${acc}');
 });
 
 module.exports = router;
